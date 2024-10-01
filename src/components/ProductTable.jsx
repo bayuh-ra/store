4659bbdd59b03a6b3906956b8091fe89ad61db08
bayuh-ra <hanclaid@gmail.com>
2024-10-01 19:37:53 +0800

    Added Filter

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -6,7 +6,6 @@ import ProductCategoryHeader from "./products/ProductCategoryHeader";
 const ProductTable = ({ headers, products }) => {
   const sportingGoods = products.filter((product) => product.type === 1);
   const electronics = products.filter((product) => product.type === 2);
-
   return (
     <div>
       <Table.TableContainer>
@@ -18,7 +17,9 @@ const ProductTable = ({ headers, products }) => {
         </Table.THead>
 
         <Table.TBody>
-          <ProductCategoryHeader text={headers[0]} />
+          {sportingGoods.length > 0 && (
+            <ProductCategoryHeader text={headers[0]} />
+          )}
 
           {sportingGoods.map((sportingGood) => (
             <ProductItem
@@ -28,7 +29,9 @@ const ProductTable = ({ headers, products }) => {
             />
           ))}
 
-          <ProductCategoryHeader text={headers[1]} />
+          {electronics.length > 0 && (
+            <ProductCategoryHeader text={headers[1]} />
+          )}
 
           {electronics.map(({ id, name, price, type }) => (
             <ProductItem key={`${type}-${id}`} name={name} price={price} />
